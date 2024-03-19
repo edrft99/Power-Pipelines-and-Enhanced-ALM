7174a8f4f8a84af890574291a4a823da170b9184
Ed Williams <70483188+edrft99@users.noreply.github.com>
2024-03-18 21:51:35 -0400

    Adding in tabcontrol

---
+++ tabcontrol.js
@@ -0,0 +1,19 @@
+function onFormLoad(executionContext) {
+  var formContext = executionContext.getFormContext();
+  // Get the record ID
+  var recordId = formContext.data.entity.getId();
+  // Set the vars
+  var tabinfo = formContext.ui.tabs.get("tab_info");
+
+  // Check if the record has been created
+  if (recordId === null || recordId === "") {
+    // If the record has not been created, hide the tabs
+    hideOrShowTabsAndSections(false);
+  } else {
+    // If the record has been created, show all tabs
+    hideOrShowTabsAndSections(true);
+  }
+  function hideOrShowTabsAndSections(isVisible) {
+    tabinfo.setVisible(isVisible);
+  }
+}
